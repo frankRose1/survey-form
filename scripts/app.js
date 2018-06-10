254d9e30c957c57f0480397ceb6d2894435742ed
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-06-09 23:59:22 -0400

    added anoher function to replace the placeholder text

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     //input fields
     const textInput = $('input[type="text"]');
     const emailInput = $('input[type="email"]');
-    const ageInput = $('input[type="age"]');
+    const ageInput = $('input[type="number"]');
 
     function shiftText(){
         const text = $(this).attr('placeholder');
@@ -12,9 +12,23 @@ $(document).ready(function(){
         const label = $('label[for="'+ id +'"]');
         $(label).text(text);
         $(this).attr('placeholder', '');
+    } 
+
+    function replaceText(){
+        const id = $(this).attr('id');
+        const label = $('label[for="'+ id +'"]');
+        const  text = label.text();
+        $(this).attr('placeholder', text);
+        label.text('');
     }
 
+    //toggle the placeholder text in and out of the input/label
     textInput.on('mouseenter', shiftText);
-    textInput.on('mouseenter', shiftText);
-    textInput.on('mouseenter', shiftText);
+    textInput.on('mouseleave', replaceText);
+
+    emailInput.on('mouseenter', shiftText);
+    emailInput.on('mouseleave', replaceText);
+
+    ageInput.on('mouseenter', shiftText);
+    ageInput.on('mouseleave', replaceText);
 });
